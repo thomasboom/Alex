93fc94b5ead7244903d6442187e2f444ef22084c
ThomasNow Productions <thomasnowprod@proton.me>
2026-01-14 21:44:41 +0100

    Fix UI

--- lib/widgets/chat_message.dart
+++ lib/widgets/chat_message.dart
@@ -1,10 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:markdown_widget/markdown_widget.dart';
 
 import '../constants/app_theme.dart';
 
-/// Widget for displaying chat messages with markdown support
 class ChatMessage extends StatelessWidget {
   final String text;
   final bool isUser;
@@ -26,7 +24,6 @@ class ChatMessage extends StatelessWidget {
       child: Stack(
         alignment: Alignment.center,
         children: [
-          // Soft glowing background - always show with dynamic color
           Container(
             decoration: BoxDecoration(
               shape: BoxShape.circle,
@@ -46,66 +43,20 @@ class ChatMessage extends StatelessWidget {
             width: 220,
             height: 220,
           ),
-          // Text content - only show when there's actual text
           if (text.isNotEmpty)
             SizedBox(
               width: MediaQuery.of(context).size.width * 0.85,
-              height: 220, // Match the background circle height
+              height: 220,
               child: Center(
-                child: MarkdownWidget(
-                  data: text,
-                  config: MarkdownConfig(
-                    configs: [
-                      PConfig(
-                        textStyle: GoogleFonts.playfairDisplay(
-                          fontSize: 28,
-                          fontWeight: FontWeight.w400,
-                          color: Theme.of(context).colorScheme.onSurface,
-                          height: 1.6,
-                        ),
-                      ),
-                      H1Config(
-                        style: GoogleFonts.playfairDisplay(
-                          fontSize: 36,
-                          fontWeight: FontWeight.w500,
-                          color: Theme.of(context).colorScheme.onSurface,
-                          height: 1.4,
-                        ),
-                      ),
-                      H2Config(
-                        style: GoogleFonts.playfairDisplay(
-                          fontSize: 32,
-                          fontWeight: FontWeight.w500,
-                          color: Theme.of(context).colorScheme.onSurface,
-                          height: 1.4,
-                        ),
-                      ),
-                      H3Config(
-                        style: GoogleFonts.playfairDisplay(
-                          fontSize: 28,
-                          fontWeight: FontWeight.w500,
-                          color: Theme.of(context).colorScheme.onSurface,
-                          height: 1.4,
-                        ),
-                      ),
-                      CodeConfig(
-                        style: GoogleFonts.playfairDisplay(
-                          fontSize: 24,
-                          fontWeight: FontWeight.w400,
-                          color: Theme.of(context).colorScheme.onSurface,
-                          height: 1.5,
-                        ),
-                      ),
-                      PreConfig(
-                        textStyle: GoogleFonts.playfairDisplay(
-                          fontSize: 22,
-                          fontWeight: FontWeight.w400,
-                          color: Theme.of(context).colorScheme.onSurface,
-                          height: 1.5,
-                        ),
-                      ),
-                    ],
+                child: Text(
+                  text,
+                  style: GoogleFonts.playfairDisplay(
+                    fontSize: 28,
+                    fontWeight: FontWeight.w400,
+                    color: Theme.of(context).colorScheme.onSurface,
+                    height: 1.6,
                   ),
+                  textAlign: TextAlign.center,
                 ),
               ),
             ),
